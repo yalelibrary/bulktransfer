94b6a41d6b2251d23e02638ef7f5673d819c8b94
osmandin <osmandin@gmail.com>
2016-05-16 15:04:02 -0400

    Externalize instructions

--- src/yale/HelpAction.java
+++ src/yale/HelpAction.java
@@ -3,11 +3,16 @@ package yale;
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.beans.PropertyChangeListener;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URL;
 
 /**
  * Help menu helper
  */
 class HelpAction implements Action {
+
     @Override
     public Object getValue(String key) {
         return null;
@@ -40,7 +45,34 @@ class HelpAction implements Action {
 
     @Override
     public void actionPerformed(ActionEvent e) {
-        JOptionPane.showMessageDialog(null, "Instructions (Mac): \n\n sudo mount -t smbfs //DOMAIN\\;netid@url/share dir \n sudo java -cp . yale.FileCopy \n\n" +
-                "Instructions (Linux -- Cent OS) \n sudo /sbin/mount.cifs //url/share dir -o user=netid, domain=");
+        /*JOptionPane.showMessageDialog(null, "Instructions (Mac): \n\n sudo mount -t smbfs //DOMAIN\\;netid@url/share dir \n sudo java -cp . yale.FileCopy \n\n" +
+                "Instructions (Linux -- Cent OS) \n sudo /sbin/mount.cifs //url/share dir -o user=netid, domain="); */
+        JOptionPane.showMessageDialog(null, getText());
     }
+
+    private String getText() {
+        final StringBuffer sb = new StringBuffer();
+        try {
+            final URL oracle =   getClass().getResource("instructions.txt");
+
+
+            BufferedReader in = new BufferedReader(
+                    new InputStreamReader(oracle.openStream()));
+
+
+            String inputLine;
+            while ((inputLine = in.readLine()) != null) {
+                sb.append(inputLine);
+                sb.append(System.getProperty("line.separator"));
+            }
+
+            in.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return sb.toString();
+    }
+
+
 }
