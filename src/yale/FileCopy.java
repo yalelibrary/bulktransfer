5c2bc9a57dd4d97b6b8f08ea59ea60ac6db4c4f4
osmandin <osmandin@gmail.com>
2016-05-25 15:30:47 -0400

    Add crawling method

--- src/yale/FileCopy.java
+++ src/yale/FileCopy.java
@@ -412,6 +412,8 @@ public class FileCopy extends JFrame implements ActionListener, PropertyChangeLi
 
         private Map<File, File> filesToCopy = new HashMap<File, File>();
 
+        private Map<String, String> filesMap = new HashMap<String, String>();
+
         private List<String> identifiers = new ArrayList<String>();
 
         public CopyTask(File source, File target) {
@@ -440,7 +442,10 @@ public class FileCopy extends JFrame implements ActionListener, PropertyChangeLi
 
             try {
                 identifiers = Arrays.asList(txtIdentifiers.getText().split("\\s*,\\s*")); //.split("\\r?\\n");
-                //gather(source, target);
+                index(source, target);
+
+                System.out.println("Index size:" + filesMap.size());
+                /*
                 if (dirs.isEmpty()) { // N.B. multiple caches?
                     detailsBox.append("Building folder cache...");
                     getFolderNames(source, target);
@@ -450,7 +455,8 @@ public class FileCopy extends JFrame implements ActionListener, PropertyChangeLi
                 }
 
                 System.out.println("Accumulated folder size:" + dirs.size());
-                checkFilesInDirs();
+                checkFilesInDirs(); */
+
                 //copyFiles();
             } catch (Exception e) {
                 logger.log(Level.WARNING, "Internal error:", e);
@@ -513,6 +519,37 @@ public class FileCopy extends JFrame implements ActionListener, PropertyChangeLi
             }
         }
 
+        /**
+         * Makes directories (if necessary) and populates a map (for later retrieval)
+         */
+        private void index(final File sourceFile, final File targetFile) throws IOException {
+            if (sourceFile.isDirectory()) {
+                final String absPath = sourceFile.getAbsolutePath();
+
+                logger.log(Level.INFO, "Looking in:{0}", absPath);
+
+                if (absPath.contains("CaptureOne") || absPath.contains(".DS_Store") || sourceFile.getName().startsWith(".") ) { // TODO
+                    return;
+                }
+                final String[] paths = sourceFile.list();
+
+                for (final String filePath : paths) {
+                    final File src = new File(sourceFile, filePath);
+                    final File dest = new File(targetFile, filePath);
+
+                    index(src, dest);
+                }
+            } else { // add file object to map
+                final String fileName = sourceFile.getName();
+
+                if (fileName.startsWith(".DS_Store") || fileName.startsWith("._.DS_Store")) {
+                    return;
+                }
+                logger.log(Level.INFO, "Put file:{0} for: {1}", new Object[]{sourceFile.getName(), sourceFile.getAbsolutePath()});
+                filesMap.put(sourceFile.getName(), sourceFile.getAbsolutePath());
+            }
+        }
+
         // Copy files stored in java.util.Map
         private void copyFiles() {
             final ExecutorService pool = Executors.newFixedThreadPool(MAX_THREADS);
@@ -539,81 +576,14 @@ public class FileCopy extends JFrame implements ActionListener, PropertyChangeLi
         }
 
 
+        private boolean downloadFile(final File f) {
+            return DownloadFile.download(f, identifiers);
+        }
+
         @Override
         public void done() {
             setProgress(100);
             btnCopy.setText("Copy");
-        }
-
-        private boolean downloadFile(final File f) {
-
-            final String fullFileName = f.getName();
-
-            // remove the file extension
-
-            int count = fullFileName.length() - fullFileName.replace(".", "").length();
-
-            if (count > 1) {
-                detailsBox.append("Unexpected file name:" + f.getAbsolutePath() + "\n");
-                return false;
-            }
-
-
-            final String fileName = fullFileName.substring(0, fullFileName.lastIndexOf('.'));
-
-            // System.out.println("Will evaluate:" + fileName);
-
-            for (final String s : identifiers) {
-
-                if (s.equals(fileName)) {
-                    return true;
-                }
-
-                if (s.contains("-")) {
-                    final String[] sp = s.split("-");
-
-                    if (sp.length == 2) {
-
-                        //strip digits
-
-                        final String fileA = sp[0].replaceAll("[^\\d.]", "");
-
-                        if (!fileA.matches("[0-9]+")) {
-                            continue;
-                        }
-
-
-                        final int fileANum = Integer.parseInt(fileA);
-                        final String fileB = sp[1].replaceAll("[^\\d.]", "");
-
-                        if (!fileB.matches("[0-9]+")) {
-                            continue;
-                        }
-
-                        final int fileBNum = Integer.parseInt(fileB);
-                        final String prefixA = sp[0].replace(sp[0], "");
-                        final String prefixB = sp[1].replace(sp[1], "");
-
-                        if (prefixA.equals(prefixB) && fileName.startsWith(prefixA)) {
-                            final String fi = fileName.replaceAll("[^\\d.]", "");
-
-                            if (!fi.isEmpty()) {
-                                if (!fi.matches("[0-9]+")) {
-                                    continue;
-                                }
-
-                                final int fileNum = Integer.parseInt(fi);
-
-                                if (fileNum >= fileANum && fileNum <= fileBNum) {
-                                    return true;
-                                }
-                            }
-                        }
-                    } // end of sp.length == 2
-                }
-            }
-
-            return false;
         }
 
         /**
